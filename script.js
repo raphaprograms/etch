ea071a8973c21a0624f36b4e52739128c8ee60e3
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-03-28 11:59:24 -0700

    Add code to manually create dynamic grid
    Logic for grid is confirmed to be correct at this stage. Next rework
    code to take input on page.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+const container = document.querySelector(".container");
+
+const userInput = 4;
+
+function drawGrid(gridSize) {
+    container.textContent = "";
+
+    const squareSize = 500 / gridSize;
+
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-item');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        container.appendChild(square);
+    }
+}
+
+drawGrid(userInput);
